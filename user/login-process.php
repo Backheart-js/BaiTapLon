b35d7f75f6ecdc0659330a9fc99716d7e506bf4b
Loannguyen123 <loannguyenthi290301@gmail.com>
2021-12-20 21:35:00 +0700

    add code user+login

---
+++ user/login-process.php
@@ -0,0 +1,52 @@
+<?php
+
+ // Taoj session: Măc định mỗi phiên làm việc có thời hạn 24 phút
+ session_strart();
+
+if(!isset($_POST['btnLogin']))
+{
+$email=$_POST['txtEmail'];
+$password=$_POST['txtPass1'];
+}
+else
+{
+    header("location:login.php");
+}
+
+
+    // Bước 01: Kết nối Database Server
+    $conn = mysqli_connect('localhost','root','12345678','project6');
+    if(!$conn){
+        die("Kết nối thất bại. Vui lòng kiểm tra lại các thông tin máy chủ");
+    }
+    // Bước 02: Thực hiện truy vấn
+    $sql = "select * FROM db_nguoidung WHERE email = '$email' and tendangnhap='$email'";
+
+    $result = mysqli_query($conn,$sql);
+
+     if(mysqli_num_rows($result) > 0){
+        
+        $row=mysqli_fetch_assoc($result);
+        $pass_hash=$row['matkhau'];//lấy các chuỗi mật khẩu băm trong bảng
+        if(password_verify($password,$pass_hash))// so sánh kiểm chứng mật khẩu thô với mật khẩu băm
+        {
+        $_SESSION['isLoginOK']=$email;
+        header("location: admin.php");//Chuyển hướng về Trang quản trị
+        }
+        else
+        {
+        $error="Email hoặc mật khẩu không chính xác";
+        header("location: login.php>error=$error"); //Chuyển hướng, hiển thị thông báo lỗi
+        }
+
+    }else{
+        $error="Email hoặc mật khẩu không chính xác";
+        header("location: login.php>error=$error"); //Chuyển hướng, hiển thị thông báo lỗi
+    }
+
+    // Bước 03: Đóng kết nối
+    mysqli_close($conn);
+?>
+
+
+
---
+++ user/login.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    <title> 
+      Flickr Login</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"/>
+    <link rel="stylesheet" href="/Assets/Css/style.css">
+  </head>
+  <body>
+    <div class="mymodal container-fluid">
+    <div class="header">
+      <div class="img_header-icon">
+          <img src="img/bieutuongfilchr.jpeg" alt="" class="header_img" />
+        </div>
+      </div>
+      <form action="process-login.php" class="form-login" method="post">
+        <div class="container modal__container">
+        <div class="modal__body">
+          <div class="modal__body-icon col-md row">
+            
+              <!---->
+              <use
+                xlink:href="/img/icons.c121c832.svg#blue-pink-balls"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+              ></use>
+            
+              <div class="form-text">
+               <svg data-v-337e9a4c="" xmlns="http://www.w3.org/2000/svg" data-v-0f36bfa4="" style="fill: currentcolor; height: 1.5rem; width: 1.5rem;"><!----><use xlink:href="/img/icons.c121c832.svg#blue-pink-balls" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg>
+                <h5 class="form-text-title ">Đăng nhập vào Flickr</h5>
+                <div class="col-md row">
+                  <div class="form__login">
+                     <div class="col-md-12 mt-3 mb-3">
+                <input
+                  type="email"
+                  class="form-control"
+                  placeholder="Địa chỉ email"  
+                  required autofocus
+                  aria-label="Recipient's username"
+                  aria-describedby="button-addon2"
+                  name="txtEmail"
+                 
+                />
+              </div>
+              <div class="col-md-12  password">
+                <input
+                  type="password"
+                  class="form-control form-cotrol-password"
+                  placeholder="Mật khẩu"
+                  required 
+                  aria-label="Recipient's username"
+                  aria-describedby="button-addon2"
+                  name="txtPass1"
+                />
+              
+                <button
+                  class="btn btn-outline-secondary"
+                  type="button"
+                  id="button-addon2"
+                >
+                <i class="fas fa-eye"></i>
+                </button>
+              </div> 
+                  </div>
+            
+              <div class="contact__agree mt-3">
+                <label >
+                    <input type="checkbox" class="checkbox" value="remember-me">
+                     Ghi nhớ địa chỉ email
+                </label>
+                <?php
+
+                if(isset($_GET['error']))
+                {
+                  echo "<h5 style ='color:red'> {$_GET['error']} </h5>";
+                }
+                ?>
+              
+             </div>
+
+              </div>
+              
+            
+              <button type="button" class="btn btn-primary" type="submit" name="btnLogin">Đăng nhập</button>
+
+            </div>
+
+            <a href="" class="form_link form_link-password mb-1">Bạn quên mật khẩu?</a>
+            <hr class="form-hr">
+            <div>
+              <p class="form_link-logup">Bạn chưa tham gia Flickr?
+                 <a href="" class="form_link ">Đăng kí tại đây.</a>
+              </p>
+             
+            </div>
+          </div>
+        </div>
+      </div>
+      </form>
+      
+    </div>
+    </div>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
