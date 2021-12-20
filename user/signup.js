c3108b815fa67beadc6603f4bd4f6eb61f4ac715
phamhuyen284 <huyenpham26112001@gmail.com>
2021-12-20 23:43:52 +0700

    fix bug connect to file .js

---
+++ user/signup.js
@@ -0,0 +1,13 @@
+// inputFirstName=document.getElementById("inputFirstName");
+// inputLastName=document.getElementById("inputLastName");
+// inputAge=document.getElementById("inputAge");
+// inputEmail=document.getElementById("inputEmail");
+
+
+alert('Hello!');
+
+// function checkInput(){
+//     if(inputFirstName.value()=='' ){
+//         alert('Bạn chưa nhập first name!');
+//     }
+// }
--- user/signup.php
+++ user/signup.php
@@ -75,6 +75,8 @@
             <p style="font-size: 12px" class="text-center lead  ">Already a Flickr member? <a href="" class="text-decoration-none">Log in here.</a></p>
           </div>
         </form>
+        <script type="text/javascript" src="signup.js" >
+        </script>
       </div>
         <div class="footer d-flex text-alight" >
           <div class="col-md-6">
@@ -95,8 +97,7 @@
   </main>
   <!-- footer -->
   
-  <script type="text/javascript" src="/javascript/signup.js" >
-  </script>
+  
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
     crossorigin="anonymous"></script>
