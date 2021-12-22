c5210a743837ead13f5c15de726fd810474c71ba
Linh <binhnguyen29122001@gmail.com>
2021-12-22 23:14:13 +0700

    edit search-navbar 22/12/2021

--- search.php
+++ search.php
@@ -8,25 +8,72 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="Assets/Css/base.css"/>
-    <link rel="stylesheet" href="Assets/Css/style.css"/>
+    <!-- <link rel="stylesheet" href="Assets/Css/style.css"/> -->
     <link rel="stylesheet" href="Assets/Css/css-navbar.css">
     <link rel="stylesheet" href="Assets/Css/responsive.css"/>
 </head>
 <body>
-    <header>
         <div class="container-fluid navbar-search-wrap">
             <div class="container navbar-search-content">
                 <div class="row ps-5 pe-5 h-100">
-                    <div class="navbar__logo-wrap col-md-6 d-flex">
-                        <a href="#" class="d-flex">
+                    <div class="navbar-search__logo-wrap col-md-5 row">
+                        <a href="#" class="col-md-3 d-flex">
                             <img class="navbar-search__logo-img" src="./Assets/data_img/flickr_demo.svg" alt="">
                         </a>
+                        <div class="navbar__cate-wrap col-md-9">
+                            <ul class="navbar__cate-list">
+                                <li class="navbar__cate-item">
+                                    <a href="" class="navbar__cate-link">Khám phá</a>
+                                    <ul class="navbar__subcate-list">
+                                        <li class="navbar__subcate-item">
+                                            <a href="" class="navbar__subcate-link">Ảnh gần đây</a>
+                                        </li>
+                                        <li class="navbar__subcate-item">
+                                            <a href="" class="navbar__subcate-link">Xu hướng</a>
+                                        </li>
+                                        <li class="navbar__subcate-item">
+                                            <a href="" class="navbar__subcate-link">Sự kiện</a>
+                                        </li>
+                                        <li class="navbar__subcate-item">
+                                            <a href="" class="navbar__subcate-link">The Common</a>
+                                        </li>
+                                        <li class="navbar__subcate-item">
+                                            <a href="" class="navbar__subcate-link">Phòng trưng bày trên Flickr</a>
+                                        </li>
+                                        <li class="navbar__subcate-item">
+                                            <a href="" class="navbar__subcate-link">Bản đồ thế giới</a>
+                                        </li>
+                                        <li class="navbar__subcate-item">
+                                            <a href="" class="navbar__subcate-link">Tìm máy ảnh</a>
+                                        </li>
+                                        <li class="navbar__subcate-item">
+                                            <a href="" class="navbar__subcate-link">Flickr Blogs</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="navbar__cate-item">
+                                    <a href="" class="navbar__cate-link">In</a>
+                                    <ul class="navbar__subcate-list">
+                                        <li class="navbar__subcate-item">
+                                            <a href="" class="navbar__subcate-link">In và hình ảnh nghệ thuật</a>
+                                        </li>
+                                        <li class="navbar__subcate-item">
+                                            <a href="" class="navbar__subcate-link">Phóng sự ảnh</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="navbar__cate-item">
+                                    <a href="" class="navbar__cate-link">Dùng Pro</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                    <div class="navbar-search__input-login col-md-6">
+                    <!-- <div class="col-md-1"></div> -->
+                    <div class="navbar-search__input-login col-md-7">
                         <div class="row h-100">
-                            <div class="navbar-search__input-wrapper col-md-6 d-flex">
+                            <div class="navbar-search__input-wrapper col-md-7 d-flex">
                                 <div class="navbar-search__input-wrap">
-                                    <span class="navbar__input-icon">
+                                    <span class="navbar-search__input-icon">
                                         <i class="fas fa-search" style="font-size: 17px; transform: translateY(3px); color: #999;"></i>
                                     </span>
                                     <span>
@@ -35,7 +82,7 @@
                                 </div>
                             </div>
     
-                            <div class="navbar-search__user-wrap d-flex col-md-6">
+                            <div class="navbar-search__user-wrap d-flex col-md-5">
                                 <div class="navbar-search__upload">
                                     <a href="" class="navbar-search__upload-link">
                                         <i class="navbar-search__upload-icon fas fa-cloud-upload-alt"></i>
@@ -59,6 +106,11 @@
         </div>
         
     
-    </header>
+
+    <div class="container-fluid search-category">
+        <div class="container">
+
+        </div>
+    </div>
 </body>
 </html>
