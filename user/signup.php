47a6e942f24be680915848921107e4fd1de5eeaa
phamhuyen284 <huyenpham26112001@gmail.com>
2021-12-25 17:10:21 +0700

    fix css for signup

--- user/signup.php
+++ user/signup.php
@@ -9,8 +9,8 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-  <!-- <link rel="stylesheet" href="/BTL/css/signup.css"> -->
-  <link rel="stylesheet" href="/Assets/Css/signup.css?v=<?php echo time(); ?>">
+  <link rel="stylesheet" href="/BTL/css/signup.css">
+  <link rel="stylesheet" href="/BaiTapLon/Assets/Css/signup.css?v=<?php echo time(); ?>">
   <script src="https://www.google.com/recaptcha/api.js" ></script>
   
 </head>
