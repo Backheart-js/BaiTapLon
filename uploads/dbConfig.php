760b72be4aba5dc8287ea43744bbe5249d50a70e
Loannguyen123 <loannguyenthi290301@gmail.com>
2022-01-01 21:35:38 +0700

    create uploads

--- uploads/dbConfig.php
+++ uploads/dbConfig.php
@@ -6,10 +6,10 @@ $dbPassword = "12345678";
 $dbName     = "BaiTapLon";
 
 // Create database connection
-$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);//tương đương sql_connect
+$db =  mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);//tương đương sql_connect
 
 // Check connection
-if ($db->connect_error) {//truy cập thuộc tính phương thức của 1 đối tượng trong php
-    die("Connection failed: " . $db->connect_error);
+if (!$db) {//truy cập thuộc tính phương thức của 1 đối tượng trong php
+    die("Connection failed: " .mysqli_connect_error());
 }
 ?>
--- uploads/index.php
+++ uploads/index.php
@@ -1,16 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>PHP upload file to sever and storage in Database</title>
-</head>
-<body>
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    Select Image File to Upload:
-    <input type="file" name="myfile">
-    <input type="submit" name="sbmUpload" value="Upload">
-</form>
-</body>
+    <link rel="stylesheet" href="/css/upload_form.css" />
+  </head>
+  <body style="margin:0;">
+    <header class="">biểu tượng</header>
+    <div class="  "style="background-color: #313131;">
+      <form
+        action="upload.php"
+        method="post"
+        enctype="multipart/form-data"
+        class="form_upload"
+        style="height :100vh;margin=0;"
+      >
+        <h3 class=""style="color:#fefefe">Bạn có thể tải thêm 1000 ảnh và video lên</h3>
+        <h5 style="color:#fefefe">Kéo & thả ảnh và video ở đây hoặc</h5>
+        <input type="file" name="myfile" class="btn btn-primary" />
+        <input type="submit" name="sbmUpload" value="Upload" />
+      </form>
+    </div>
+  </body>
 </html>
