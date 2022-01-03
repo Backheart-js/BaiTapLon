df02f736f6b81d2b857c1674f162b9f26745daa2
Loannguyen123 <loannguyenthi290301@gmail.com>
2022-01-03 15:32:59 +0700

    show sucess

--- uploads/show.php
+++ uploads/show.php
@@ -3,13 +3,16 @@
 include 'dbConfig.php';
 
 // Get images from the database
-$result = mysqli_connect_error("SELECT * FROM db_images ORDER BY uploaded_on DESC");
+$sql = "SELECT * FROM db_images ORDER BY uploaded_on DESC"; 
+$result=mysqli_query($db,$sql);
 
 if( mysqli_num_rows($result) > 0){
     while($row = mysqli_fetch_assoc($result)){
-        $imageURL = 'uploads/'.$row["file_name"];
+        $imageURL = ('./../uploads/') . '/'.$row["file_name"];
+        $i++;
 ?>
-    <img src="<?php echo $imageURL; ?>" alt="" />
+    <img src="<?php echo $imageURL; ?> " alt="" style='width: 100%;height: 200px;'/>
+    <br/><br/>
 <?php }
 }else{ ?>
     <p>No image(s) found...</p>
--- uploads/upload.php
+++ uploads/upload.php
@@ -2,15 +2,20 @@
 // import cấu hình bước 1
 require 'dbConfig.php';
 $statusMsg = '';//tạo ra 1 biến để lưu lại trang thái upload nhằm phản hồi cho người dùng
-
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
 // Những động tác về thiết lập cho việc chuẩn bị upload
-$targetDir = "uploads/";//Thư mục chỉ định để lưu trữ tệp tải lên;
+$targetDir = ('./../uploads/') . '/';//Thư mục chỉ định để lưu trữ tệp tải lên;
 $fileName = basename($_FILES["myfile"]["name"]);//$_file biến siêu toàn cục
 $targetFilePath = $targetDir . $fileName;//đây là tên đầy đủ+đường dẫn tệp tin lưu trữ toàn bộ phần tử
 $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);//đang bắt định dạng tệp tin
 //Bước 2: Kiểm tra người dùng đã bấm submit và file đã được chọn chưa
 if(isset($_POST["sbmUpload"]) && !empty($_FILES["myfile"]["name"])){
      
+
+
+
     if (file_exists($targetFilePath)) {
         echo "Tệp tin đã tồn tại";
     }
@@ -19,28 +24,28 @@ if(isset($_POST["sbmUpload"]) && !empty($_FILES["myfile"]["name"])){
     $allowTypes = array('jpg','png','jpeg','gif','pdf');
     if(in_array($fileType, $allowTypes)){//Kiểm tra 1 giá trị có thuộc mảng không
         //xử lí upload đang lưu ở thư mục tạm
-
-
-        $tagetFile = "/uploads/" . $fileName;
-        var_dump($tagetFile);
-        copy($_FILES["myfile"]["tmp_name"], $tagetFile);
-        echo "<img src='".$tagetFile."'>";
+        $tagetFile = $targetDir . $fileName;
         if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $tagetFile)){//Lấy từ nơi tạm đẩy vào nơi chính
             // Lưu đường dẫn vào cơ sở dữ liệu
-          echo"1"; die;
-            $sql = "INSERT into db_images (file_name, uploaded_on) VALUES ('".$fileName."', NOW())"; 
-            echo $sql;
-            $insert=mysqli_query($db,$sql);
-            if($insert){ //kiểm tra việc query thành công
-                $statusMsg = "The file ".$fileName. " has been uploaded successfully.";
-                header("location:show.php");
-            }else{
-                $statusMsg = "File upload failed, please try again.";
-            } 
+            //echo $fileName;die;
+
+
+            
+            try{
+                $sql = "INSERT into db_images (file_name, uploaded_on) VALUES ('".$fileName."', NOW())"; 
+            //echo $sql;die;
+                $insert=mysqli_query($db,$sql);
+                if($insert){ //kiểm tra việc query thành công
+                    $statusMsg = "The file ".$fileName. " has been uploaded successfully.";
+                    header("location:show.php");
+                }else{
+                    $statusMsg = "File upload failed, please try again.";
+                } 
+            }catch(\Exception $e){
+            }
         }
-        else{     
-            var_dump($_FILES["myfile"]["error"]);  
-            echo"2"; die;
+        else{      
+            echo"<br>error"; die;
             $statusMsg = "Sorry, there was an error uploading your file.";
         }
     }
