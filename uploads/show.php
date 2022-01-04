d369c8d40f8b3573fa41e6bf89c1e187f4fb3b30
Loannguyen123 <loannguyenthi290301@gmail.com>
2022-01-04 21:22:19 +0700

    chinh sua cho anh nho lai

--- uploads/show.php
+++ uploads/show.php
@@ -5,14 +5,20 @@ include 'dbConfig.php';
 // Get images from the database
 $sql = "SELECT * FROM db_images ORDER BY uploaded_on DESC"; 
 $result=mysqli_query($db,$sql);
+?>
+<div style="display:flex;">
+<?php
 
 if( mysqli_num_rows($result) > 0){
     while($row = mysqli_fetch_assoc($result)){
         $imageURL = ('./../uploads/') . '/'.$row["file_name"];
         $i++;
+        
 ?>
-    <img src="<?php echo $imageURL; ?> " alt="" style='width: 100%;height: 200px;'/>
-    <br/><br/>
+   <img src="<?php echo $imageURL; ?> " alt="" style='width:10%;'/>
+</div>
+    
+    
 <?php }
 }else{ ?>
     <p>No image(s) found...</p>
